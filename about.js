c115b8569e4193fa6251873bd892b4e9df000b88
up-mak <fcitra920@gmail.com>
2024-11-27 10:45:44 +0800

    upmak

---
+++ about.js
@@ -0,0 +1,49 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. 
+    addEventListener("click", () => {
+    hamburger.classList.remove("active");    
+    navMenu.classList.remove("active");    
+}))
+
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoins: {
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        }
+    }
+
+  });
+
+
+  var mixer = mixitup('.wrap-gallery');
